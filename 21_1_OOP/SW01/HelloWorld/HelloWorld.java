c6ce8c0878cd7afa4a60bb36f8bcc01bac0d4e60
Luca Strebel <luca.strebel@jmc-software.ch>
2021-02-22 19:28:17 +0100

    move files and add HelloWorld

---
+++ 21_1_OOP/SW01/HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+* OOP-Intro.
+*/
+public final class HelloWorld {
+    /**
+    * @param args not used.
+    */
+    public static void main(final String[] args) {
+        System.out.println("Haben Sie Fragen?");
+    }
+}
